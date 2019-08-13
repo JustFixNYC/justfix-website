08ac5e8be41f8bcbf0c7a8523f55a429daba9b46
Sam Raby <Sam@Sams-MBP-4.fios-router.home>
2019-08-13 14:49:22 -0400

    added scaffolding for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,22 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const LandingPage  = () => (
-<StaticQuery
-    query={graphql`
-      query {
-      }
-    `}
-  render = {data => (<Layout>
-    {/* <div id="home" className="home-page" contentful-entry="'content_type=homePage'">
+class LandingPageScaffolding extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+  console.log(this.props);
+  return (
+  <Layout>
+    <div id="home" className="home-page" contentful-entry="'content_type=homePage'">
 
       <section className="img-bg hero">
         <article className="container">
           <div className="hero-container">
-            <h1>{{ $contentfulEntry.fields.heroCopy }}</h1>
-            <p className="subcopy hidden-mobile">{{$contentfulEntry.fields.subCopy}}</p>
+            <h1>{this.props.data.heroCopy.heroCopy}</h1>
+            <p className="subcopy hidden-mobile">{this.props.data.subCopy.subCopy}</p>
 
             <a href="#" scroll-to="products" className="btn btn-outline">
               LEARN MORE
@@ -34,31 +36,35 @@ const LandingPage  = () => (
 
         <h1 className="text-center">Our Products & Services</h1>
 
-        <div className="row" ng-repeat="entry in $contentfulEntry.fields.homePageProductBlocks track by $index">
-            <div className="col-sm-6 col-xs-12" ng-className="{ 'col-sm-push-6': $index % 2 == 0 }">
-              <img ng-src="{{entry.fields.screenshot.fields.file.url}}" />
+        { (this.props.data.homePageProductBlocks).map( (product, i) =>
+          (<div className="row" key={i}>
+            <div className="col-sm-6 col-xs-12">
+              <img src={product.screenshot.file.url} />
             </div>
-            <div className="col-sm-6 col-xs-12" ng-className="{ 'col-sm-pull-6': $index % 2 == 0 }">
+            <div className="col-sm-6 col-xs-12">
               <div className="product-content-block">
-                <h3>{{entry.fields.title}}</h3>
-                <p>{{entry.fields.description}}</p>
-                <a className="btn btn-primary btn-block" href="{{entry.fields.cta.url}}?utm_source=orgsite" target="_blank">{{entry.fields.cta.title}}</a>
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+                <a className="btn btn-primary btn-block" href={ (product.cta.url + "?utm_source=orgsite") } target="_blank">{product.cta.title}</a>
               </div>
             </div>
-        </div>
+          </div>) )
+        }
 
       </section>
+
       <section className="justfix-blue no-margin center white padding-section-reduced " id="rental-history">
         <div className="container">
           <h3>Want your apartment’s Rental History?</h3>
           <p>This can help you find out if you are being overcharged on rent. Text <span className="semi-bold">“RENT HISTORY”</span> to (646) 783-0627 and get your Rental History from the DHCR in the mail — <i>¡Tambien disponible in Español!</i></p>
         </div>
       </section>
+
       <section className="video_unit padding-section-reduced gray_bg">
         <div className="container">
           <div className="row">
             <div className="col-sm-12">
-              <h1 className="video_unit-title">{{$contentfulEntry.fields.videoUnit.fields.title}}</h1>
+              <h1 className="video_unit-title">{this.props.data.videoUnit.title}</h1>
             </div>
           </div>
           <div className="row align_items_end-sm">
@@ -68,7 +74,7 @@ const LandingPage  = () => (
               </div>
             </div>
             <div className="col-sm-4">
-              <p className="video_unit-caption">{{$contentfulEntry.fields.videoUnit.fields.caption}}</p>
+              <p className="video_unit-caption">{this.props.data.videoUnit.caption.caption}</p>
               <a ui-sref="mission" className="btn btn-outline black video_unit-button">READ OUR MISSION</a>
             </div>
           </div>
@@ -76,22 +82,65 @@ const LandingPage  = () => (
       </section>
 
       <section className="padding-section-reduced">
-        <h1 className="center no-margin-bottom">{{$contentfulEntry.fields.pressTitle}}</h1>
+        <h1 className="center no-margin-bottom">{this.props.data.pressTitle}</h1>
         <div className="container logos logos--faded">
-          <span
-            className="logo"
-            ng-repeat="pressLogo in $contentfulEntry.fields.pressLogos"
-            >
-            <div className="background-image"
-              ng-style="{'background-image': 'url(' + pressLogo.fields.logo.fields.file.url + ')'}">
-            </div>
-          </span>
+        { (this.props.data.pressLogos).map( (logo,i) =>
+          (<span className="logo" key={i}>
+          <div className="background-image" style = { { backgroundImage: "url(" + logo.logo.file.url + ")"  } } >
+          </div>
+        </span>) )
+        }
+
         </div>
       </section>
 
-      </div> */}
-  </Layout>
-  )}
+      </div>
+  </Layout>) 
+  }
+}
+
+const LandingPage  = () => (
+<StaticQuery
+    query={graphql`
+      query {
+        contentfulHomePage {
+          heroCopy {
+            heroCopy
+          }
+          subCopy {
+            subCopy
+          }
+          homePageProductBlocks {
+            title
+            description
+            cta {
+              title
+              url
+            }
+            screenshot {
+              file {
+                url
+              }
+            }
+          }
+          videoUnit {
+            title
+            caption {
+              caption
+            }
+          }
+          pressTitle 
+          pressLogos {
+            logo {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+  render = {data => (<LandingPageScaffolding data={data.contentfulHomePage} />)}
   />
 );
 
