476e1ac66284abb63fdb15956bcd57308fb7fae3
Sam Raby <sraby@users.noreply.github.com>
2019-09-12 15:50:54 -0400

    Added site plugins (#23)
    * added gtm and ga plugins
    
    * updated yarn.lock
    
    * added canonicals

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ const contentfulConfig = {
   accessToken: process.env.ACCESS_TOKEN,
 }
 
+const tracking = {
+  gtm: "GTM-NMPT5JP",
+  ga: "UA-67069242-1",
+  rollbar: "61296a1fcbc94b408bf1c1e71a851794",
+  heap: "3368297951",
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Contentful TypeScript starter',
@@ -22,13 +29,41 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/img/brand/favicon-96x96.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: process.env.SITE_URL,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: tracking.gtm,
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: tracking.ga,
       },
     },
     {
       resolve: "gatsby-plugin-rollbar",
       options: {
-        accessToken: "61296a1fcbc94b408bf1c1e71a851794",
+        accessToken: tracking.rollbar,
         // For all configuration options, see https://docs.rollbar.com/v1.0.0/docs/rollbarjs-configuration-reference
         captureUncaught: true,
         captureUnhandledRejections: true,
@@ -40,7 +75,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-heap',
       options: {
-        appId: '3368297951',
+        appId: tracking.heap,
         enableOnDevMode: false // if 'false', heap will be fired on NODE_ENV=production only
       },
     },
