b441fdcb34192b4537c2eb3e74925785c259a4a5
Sam Raby <Sam@Sams-MBP-4.fios-router.home>
2019-09-23 17:23:40 -0400

    added subscribe page

---
+++ src/pages/subscribed.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import Layout from '../components/layout'
+
+
+const SubscribedPageScaffolding = (props) => 
+  (<Layout metadata={{title:"Subscribed"}}>
+    <section className="hero is-small is-white">
+        <div className="hero-body has-text-centered is-horizontal-center">
+            <div className="content content-wrapper tight">
+                <h1 className="title is-size-2 has-text-grey-dark has-text-weight-normal is-spaced">
+                    {props.content.title}
+                </h1>
+                <span className="is-size-5">
+                    {documentToReactComponents(props.content.description.json)}
+                </span>
+            </div>
+                <br />
+            <div className="container content-wrapper tight">
+                <figure className="image">
+                    <img src={props.content.teamPhoto.file.url} />
+                </figure>
+                    <br />
+                <p className="title is-size-5 has-text-weight-normal is-italic is-spaced">
+                    {props.content.teamPhotoTitle}
+                </p>
+            </div>
+        </div>
+    </section>
+  </Layout>); 
+
+const TermsOfUsePage  = () => (
+<StaticQuery
+    query={graphql`
+      query {
+        contentfulSubscriptionConfirmationPage {
+            title
+            description {
+                json
+            }
+            teamPhotoTitle
+            teamPhoto {
+                file {
+                    url
+                }
+            }
+        }
+      }
+    `}
+  render = {data => (<SubscribedPageScaffolding content={data.contentfulSubscriptionConfirmationPage} />)}
+  />
+);
+
+export default TermsOfUsePage;
