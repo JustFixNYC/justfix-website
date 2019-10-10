a2c214e2386ffa324d3448c09f90348756d7ff2f
Atul Varma <varmaa@gmail.com>
2019-10-10 11:44:05 -0400

    WHY IS THIS SO HARD

---
+++ src/components/ddo-searchbar.test.tsx
@@ -0,0 +1,11 @@
+import { getDDOURL } from './ddo-searchbar';
+
+describe('getDDOURL()', () => {
+  it('includes only address if borough is not provided', () => {
+    expect(getDDOURL({address: 'blarg', borough: null}, 'http://boop/')).toBe('http://boop/?address=blarg');
+  });
+
+  it('includes address and borough if both are provided', () => {
+    expect(getDDOURL({address: 'blarg', borough: 'BRONX'}, 'http://boop/')).toBe('http://boop/?address=blarg&borough=BRONX');
+  });
+});
--- src/example.test.tsx
+++
@@ -1,5 +0,0 @@
-describe("Test suite", () => {
-  it("should run", () => {
-    expect(1).toBe(1);
-  });
-});
