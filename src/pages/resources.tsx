bdf312c6a5c383a72bee51f666a751b57aaf34c0
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-11-21 12:01:54 -0500

    added resources landing page

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,11 +1,73 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import Img from "gatsby-image/withIEPolyfill"
+
+import '../styles/resources.scss' 
 
 import Layout from '../components/layout'
 import { ContentfulContent } from '../components/types';
 
+
+export function nameToSlug(name: string): string {
+  return name.replace(' ','-').toLowerCase()
+}
+
+type Category = {
+  title: string,
+  description: string
+}
+
 const LearningPageScaffolding = (props: ContentfulContent) => 
   (<Layout metadata={props.content.metadata}>
+    <div id="resources" className="resources-page" >
+      <section className="hero is-small is-white">
+        <div className="hero-body has-text-centered is-horizontal-center">
+          <figure className="image is-128x128 is-horizontal-center">
+            <Img fluid={props.content.headerImage.fluid} />
+          </figure>
+          <div className="container">
+            <h1 className="title is-size-2 has-text-grey-dark has-text-weight-normal">
+            {props.content.title}
+            </h1>
+            <h6 className="subtitle has-text-grey-dark is-italic">
+            {props.content.subtitle}
+            </h6>
+            <div className="buttons">
+              {(props.content.categoryButtons).map( 
+                (category: Category, i: number) =>
+                <Link key={i} to={'/resources/' + nameToSlug(category.title)}>
+                  <button className="button is-primary is-rounded is-uppercase">
+                    {category.title}
+                  </button>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="content-wrapper tight">
+        {(props.content.articles).map(
+          (article: any, i: number) => 
+          <div key={i} className="box article-preview">
+            <div className="tags">
+              <span className="tag is-primary is-light">{article.category.title}</span>
+            </div>
+            
+              <h1 className="title is-size-3 has-text-primary is-spaced has-text-weight-semibold">
+                <Link to={'/resources/' + article.slug}>
+                  {article.title}
+                </Link>
+              </h1>
+              <h6 className="subtitle has-text-grey-dark">
+                {article.previewText.previewText}
+              </h6>
+              <Link to={'/resources/' + article.slug} className="has-text-weight-semibold">
+                Read More →
+              </Link>
+          </div>
+        )}
+      </section>
+    </div>
 
   </Layout>); 
 
@@ -30,7 +92,7 @@ const LearningPage  = () => (
           subtitle
           headerImage {
             fluid {
-              src
+              ...GatsbyContentfulFluid
             }
           }
           categoryButtons {
