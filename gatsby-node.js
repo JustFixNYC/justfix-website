833d26a5eb267a6aae066d5a808f8ad84819d124
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-11-21 16:31:51 -0500

    added auto-generated article and category pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,12 +4,102 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = async function({ actions, graphql }) {
+  
+  /* Generate Learning Center pages */
+  const { data } = await graphql(`
+    query {
+      contentfulLearningCenterSearchPage {
+        categoryButtons {
+          title
+          description
+        }
+        articles {
+          metadata {
+            title
+            description
+            keywords {
+              keywords
+            }
+            shareImage {
+              file {
+                url
+              }
+            }
+          }
+          slug
+          title
+          subtitle {
+            json
+          }
+          previewText {
+            previewText
+          }
+          author
+          dateUpdated
+          categories {
+            title
+            description
+          }
+          articleSections {
+            __typename
+            ... on ContentfulLearningArticleCtaBlock {
+              title
+              subtitle
+              ctaText
+              ctaLink
+            }
+            ... on ContentfulLearningArticleSection {
+              title
+              content {
+                json
+              }
+            }
+          }
+        }
+        learningCenterCta {
+          title
+          subtitle
+          ctaText
+          ctaLink
+        }
+        justFixCta {
+          title
+          subtitle
+          ctaText
+          ctaLink
+        }
+      }
+    }
+  `)
 
-/** Redirect donation page to external portal */
-exports.createPages = ({ graphql, actions }) => {
-    const {createRedirect} = actions //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
-    createRedirect({ fromPath: '/donate', toPath: 'https://donorbox.org/donate-to-justfix-nyc', isPermanent: true });
-    createRedirect({ fromPath: '/get-repairs', toPath: '/', isPermanent: true });
-    createRedirect({ fromPath: '/about/products-and-services', toPath: '/#products', isPermanent: true });
-  }
+  /* Learning Center category pages */
+  const articles = data.contentfulLearningCenterSearchPage.articles;
+  data.contentfulLearningCenterSearchPage.categoryButtons.forEach(category => {
+    actions.createPage({
+      path: '/resources/' + category.title.replace(' ','-').toLowerCase(),
+      component: require.resolve(`./src/components/article.tsx`),
+      context: { 
+        content: category,
+        articles: articles
+       },
+    })
+  })
+
+  /* Learning Center article pages */
+  data.contentfulLearningCenterSearchPage.articles.forEach(article => {
+    actions.createPage({
+      path: '/resources/' + article.slug,
+      component: require.resolve(`./src/components/article.tsx`),
+      context: { content: article },
+    })
+  })
+
+  /* Redirects for old site pages */
+  const {createRedirect} = actions //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
+  createRedirect({ fromPath: '/donate', toPath: 'https://donorbox.org/donate-to-justfix-nyc', isPermanent: true });
+  createRedirect({ fromPath: '/get-repairs', toPath: '/', isPermanent: true });
+  createRedirect({ fromPath: '/about/products-and-services', toPath: '/#products', isPermanent: true });
+
+}
+
---
+++ src/components/article.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const LearningArticle = (content: any) => <div>Article!</div> 
+
+export default LearningArticle;
