6240303b79f95b3c1c6a7819af13ca4fbf96111d
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-11-26 16:22:44 -0500

    added scaffolding and styling for article sections

--- src/components/learning-center/article-template.tsx
+++ src/components/learning-center/article-template.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { LearningArticleFooter } from './article-footer';
+import Layout from '../layout';
+import { Link } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+import '../../styles/resources.scss' 
 
 type Props = {
     pageContext: { 
@@ -9,12 +14,118 @@ type Props = {
     }
 }
 
+/* 
+
+GraphQL Content:
+
+metadata {
+    title
+    description
+    keywords {
+        keywords
+    }
+    shareImage {
+        file {
+        url
+        }
+    }
+}
+slug
+title
+subtitle {
+    json
+}
+previewText {
+    previewText
+}
+author
+dateUpdated
+categories {
+    title
+    description
+    slug
+}
+articleSections {
+    __typename
+    ... on ContentfulLearningArticleCtaBlock {
+        title
+        subtitle
+        ctaText
+        ctaLink
+    }
+    ... on ContentfulLearningArticleSection {
+        title
+        content {
+            json
+        }
+}
+*/
+
+const renderSection = (articleSection: any, i: number) =>
+    (articleSection.__typename === "ContentfulLearningArticleCtaBlock" ? 
+        <div key={i} className="article-section" /> :
+        <div key={i} className="article-section">
+            <div className="content">
+                <h1 className="title is-size-2 has-text-grey-dark has-text-weight-semibold is-spaced">
+                    {articleSection.title}
+                </h1>
+                <p>
+                    {documentToReactComponents(articleSection.content.json)}
+                </p>
+            </div>
+        </div>
+    )
+
 const LearningArticle = (props: Props) => {
     const content = props.pageContext.content;
     return (
-        <div>Article: {content.title} 
-            <LearningArticleFooter />
-        </div>
+        <Layout metadata={content.metadata}>
+            <div className="article-page">
+                <div className="columns is-desktop">
+                    <div className="column">
+                        First column
+                    </div>
+                    <div className="column is-half-desktop">
+                        <div className="hero is-white">
+                            <div className="hero-body">
+                                <nav className="breadcrumb" aria-label="breadcrumbs">
+                                    <ul>
+                                        <li><Link to="/">JustFix.nyc</Link></li>
+                                        <li><Link to="/resources/">Resource Center</Link></li>
+                                        <li><Link to={"/resources/category/" + content.categories[0].slug + "/"}>{content.categories[0].title}</Link></li>
+                                    </ul>
+                                </nav>
+                                <div className="container content-wrapper">
+                                    <h1 className="title is-size-1 has-text-grey-dark has-text-weight-semibold is-spaced">
+                                        {content.title}
+                                    </h1>
+                                    <p className="subtitle is-size-6 has-text-grey-dark">
+                                        <span className="is-size-5">Written by {content.author}</span> 
+                                            <br/>
+                                        <span className="is-size-7">Updated {content.dateUpdated}</span> 
+                                    </p>
+                                    <p className="is-size-6 has-text-grey-dark">
+                                        {documentToReactComponents(content.subtitle.json)}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="hero-body">
+                                <div className="container content-wrapper">
+                                    {(content.articleSections).map(
+                                        (articleSection: any, i: number) => renderSection(articleSection, i)
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="column">
+                        Third column
+                    </div>
+                </div>
+                <LearningArticleFooter />
+            </div>
+        </Layout>
         
     )
 } 
