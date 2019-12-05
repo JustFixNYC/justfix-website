6406a23cdc89c5b6417df94c447b3087a4e9700e
Atul Varma <varmaa@gmail.com>
2019-12-05 11:04:01 -0500

    Use .is-uppercase on 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
   <Layout metadata={{title:"Page Not Found"}}>
     <section className="hero is-large has-background-info">
       <div className="hero-body has-text-centered">
-        <h1 className="title has-text-danger"><Trans>NOT FOUND</Trans></h1>
+        <h1 className="title has-text-danger is-uppercase"><Trans>Not found</Trans></h1>
         <p className="subtitle has-text-white"><Trans>You just found a page that doesn&#39;t exist... the sadness.</Trans></p>
       </div>
     </section>
