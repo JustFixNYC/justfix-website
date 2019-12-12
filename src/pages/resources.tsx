5971b8ba1529947ee9e1d277ee8d3116c862b6e3
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-12-12 11:11:51 -0500

    reverted all elasticlunr searchbar commits

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -7,7 +7,6 @@ import Layout from '../components/layout'
 import { ContentfulContent } from '.'
 import { LearningCenterImage } from '../components/learning-center/header-image'
 import { ThankYouBanner } from '../components/learning-center/thank-you-banner'
-import LearningCenterSearchBar from '../components/learning-center/search'
 
 export type Category = {
   title: string,
@@ -58,8 +57,6 @@ const LearningPageScaffolding = (props: ContentfulContent) =>
             <h6 className="subtitle has-text-grey-dark is-italic">
               {props.content.subtitle}
             </h6>
-            <LearningCenterSearchBar />
-              <br />
             <div className="field is-centered">
               {(props.content.categoryButtons).map( 
                 (category: Category, i: number) =>
