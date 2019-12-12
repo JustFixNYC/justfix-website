144abbc0ee6ace47917e8e17d02f0d660b090fb5
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-12-12 14:31:59 -0500

    removed ThankYouBanner import from article page

--- src/components/learning-center/article-template.tsx
+++ src/components/learning-center/article-template.tsx
@@ -6,7 +6,6 @@ import { Link as ScrollLink} from "react-scroll";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import '../../styles/resources.scss' 
-import { ThankYouBanner } from './thank-you-banner';
 
 type Props = {
     pageContext: { 
