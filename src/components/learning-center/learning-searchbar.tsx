3f00408ee2ef9463f3011088ba379ed62e6f8f2b
Sam Raby <sraby@users.noreply.github.com>
2019-12-12 11:16:16 -0500

    [WIP] Add Algolia-driven search bar to Learning Center (#70)
    * added algolia dependencies
    
    * updated env sample
    
    * added algolia search key to env sample
    
    * added algolia plugin to generate and update index on gatsby build
    
    * added env flag to enable algolia plugin conditionally
    
    * made Algolia Admin Key optional and factored out ENABLE_ALGOLIA_INDEXING var
    
    * added preliminary algolia search bar
    
    * updated algolia backend to change structure of index
    
    * tweaked structure of search bar
    
    * seemed to find a solution for customizing SearchBox
    
    * finished adding styling to search bar
    
    * Added type assertion to output of connectSearchBox to fix ts error
    
    * removed unneeded prop from CustomSearchBox component

---
+++ src/components/learning-center/learning-searchbar.tsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, Configure, Snippet, connectSearchBox, connectHits } from 'react-instantsearch-dom';
+import { Link } from 'gatsby';
+import { SearchBoxExposed } from 'react-instantsearch-core';
+
+const appId = process.env.GATSBY_ALGOLIA_APP_ID;
+const searchKey = process.env.GATSBY_ALGOLIA_SEARCH_KEY;
+
+const SearchBox = ({ currentRefinement, refine, updateSearchQuery }: any) => (
+  <form className="control" noValidate action="" role="search">
+    <input
+      className="input is-primary"
+      type="search"
+      placeholder="🔎 Search articles..."
+      value={currentRefinement}
+      onChange={event => {refine(event.currentTarget.value); updateSearchQuery(event.currentTarget.value);}}
+    />
+  </form>
+);
+
+const Hits = ({ hits }: any) => (
+  (hits && hits.length > 0 ? 
+  <div className="dropdown-content">
+    {hits.map( (hit: any) => (
+      <Link key={hit.slug} to={"/resources/" + hit.slug} className="dropdown-item">
+        <div className="is-size-6 has-text-primary has-text-weight-semibold">
+          {hit.title} →
+        </div>
+        <div className="result__snippet">
+          <Snippet
+            attribute="articleContent"
+            hit={hit}
+            tagName="u"
+          />
+        </div>
+      </Link>
+    ))}
+  </div>: <span>No results</span>)
+);
+
+/* 
+NOTE: We are including a type assertion here because the official type definition 
+of connectSearchBox does not allow us to pass additional props from the 
+input component (SearchBox) to the output component (CustomSearchBox) */
+
+const CustomSearchBox = connectSearchBox(SearchBox) as React.ComponentClass<SearchBoxExposed & any> 
+const CustomHits = connectHits(Hits);
+
+type Props = any;
+type State = { query: string };
+
+class LearningSearchBar extends Component<Props,State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
+
+  render() {
+    return (appId && searchKey ? 
+    <InstantSearch
+      searchClient={algoliasearch(appId,searchKey)}
+      indexName="learning_center"
+      resultsState={[]}
+    >
+      
+      <CustomSearchBox updateSearchQuery={(e: any) => this.setState({query: e})} />
+
+        {(this.state.query || '').length > 0 && 
+          (
+            <React.Fragment>
+              <Configure attributesToSnippet={['articleContent']} />
+              <CustomHits />
+            </React.Fragment>
+          )
+        }
+
+      </InstantSearch>: 
+    <React.Fragment />
+    )
+  }
+} 
+
+export default LearningSearchBar;
---
+++ src/util/algolia.js
@@ -0,0 +1,52 @@
+
+const pt = require('@contentful/rich-text-plain-text-renderer');
+
+const pageQuery = `{
+    articles: allContentfulLearningArticlePage(filter:{node_locale:{eq:"en-US"}}) {
+        edges {
+          node {
+            slug
+            title
+            author
+            subtitle {
+              json
+            }
+            articleSections {
+              __typename
+              ... on ContentfulLearningArticleSection {
+                title
+                content {
+                  json
+                }
+              }
+            }
+          }
+        }
+      }
+  }`
+
+  /** Take the object from the GraphQL query and 
+   * unpack and reformat data from the subtitle and article sections to create a neat array */
+  const flatten = arr =>
+    arr.map(
+      ({ node: {author, subtitle, articleSections, ...rest } }) => {
+        const byline = author ? "By " + author + " | " : "";
+        const subtitleContent = subtitle && subtitle.json ? pt.documentToPlainTextString(subtitle.json) : "";
+        const sectionsContent = articleSections.map( 
+          section => section.title && section.content && section.content.json ? section.title + " | " + pt.documentToPlainTextString(section.content.json) : "");
+        const articleContent = byline + subtitleContent + " " + sectionsContent.reduce( (section1, section2) => section1 + "     " + section2 )
+      return ({
+        ...rest,
+        articleContent,
+        });
+      })
+      
+  const queries = [
+    {
+      query: pageQuery,
+      transformer: ({ data }) => flatten(data.articles.edges),
+      indexName: `learning_center`,
+    },
+  ]
+
+  module.exports = queries
