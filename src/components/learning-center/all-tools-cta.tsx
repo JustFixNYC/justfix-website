8908be176a521de7bab49cf208ff968409224b2a
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-01-13 16:09:50 -0500

    fixed cc error with unneeded import

--- src/components/learning-center/all-tools-cta.tsx
+++ src/components/learning-center/all-tools-cta.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery, graphql, Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 import { ContentfulContent } from '../../pages';
 import { DDOSearchBar } from '../ddo-searchbar';
 
