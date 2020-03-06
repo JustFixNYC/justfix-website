c5421e2b7827c08ff951427db0a5e68372cd65b7
Atul Varma <varmaa@gmail.com>
2020-03-06 08:55:18 -0500

    Properly internationalize dates.

--- src/pages/changes.tsx
+++ src/pages/changes.tsx
@@ -15,42 +15,29 @@ type ChangelogEntry = {
   node_locale: string,
 };
 
-type YearAndMonth = {year: number, month: number};
+type YearAndMonth = {year: number, month: number, localeString: string};
 
 /**
  * Given a date string in the form "YYYY-MM-DD", returns the "YYYY-MM" part.
  */
 const isSameYearAndMonth = (a: YearAndMonth, b: YearAndMonth) => a.year === b.year && a.month === b.month;
 
-const parseYearAndMonth = (date: string): YearAndMonth => {
+const parseYearAndMonth = (date: string, locale: string): YearAndMonth => {
   const year = parseInt(date.slice(0, 4), 10);
   const month = parseInt(date.slice(5, 7), 10);
+  const dateObj = new Date(year, month - 1);
+  const localeString = dateObj.toLocaleString(locale, {month: 'long', year: 'numeric'});
 
-  return {year, month};
-};
-
-const MONTHS = {
-  1: 'January',
-  2: 'February',
-  3: 'March',
-  4: 'April',
-  5: 'May',
-  6: 'June',
-  7: 'July',
-  8: 'August',
-  9: 'September',
-  10: 'October',
-  11: 'November',
-  12: 'December',
+  return {year, month, localeString};
 };
 
 const ChangelogEntry: React.FC<{node: ChangelogEntry, prevNode?: ChangelogEntry}> = ({node, prevNode}) => {
-  const currYM = parseYearAndMonth(node.date);
-  const prevYM = prevNode ? parseYearAndMonth(prevNode.date) : undefined;
+  const currYM = parseYearAndMonth(node.date, node.node_locale);
+  const prevYM = prevNode ? parseYearAndMonth(prevNode.date, prevNode.node_locale) : undefined;
   const showYM = !prevYM || !isSameYearAndMonth(currYM, prevYM);
 
   return <>
-    {showYM && <h2 className="date-and-month">{MONTHS[currYM.month]} {currYM.year}</h2>}
+    {showYM && <h2 className="date-and-month">{currYM.localeString}</h2>}
     <h3>{node.title}</h3>
     {documentToReactComponents(node.body.json)}
   </>;
