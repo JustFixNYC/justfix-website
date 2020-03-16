c53ec9ae9c949c4746780391e1a683c316ef067e
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-16 16:30:27 -0400

    refactored team page query

--- src/pages/about/team.es.tsx
+++ src/pages/about/team.es.tsx
@@ -3,63 +3,10 @@ import { StaticQuery, graphql } from 'gatsby'
 import { TeamPageScaffolding } from './team';
 
 const TeamPage  = () => (
-<StaticQuery
-    query={graphql`
-      query {
-        contentfulTeamPage( node_locale: { eq: "es" } ) {
-          metadata {
-            title
-            description
-            keywords { 
-              keywords 
-            }
-            shareImage {
-              file {
-                url
-              }
-            }
-          }
-          title
-          teamMembers {
-            name
-            title
-            description {
-              description
-            } 
-            photo {
-              fluid {
-                src
-              }
-            }
-            professionalLinks
-          }
-          directorsTitle 
-          directors {
-            name
-            title
-            photo {
-              file {
-                url
-              }
-            }
-            organization
-            organizationLink
-            description {
-              description
-            } 
-          }
-          otherContributorsTitle
-          otherContributors {
-            name
-            link
-          }
-          readMore {
-            title
-            link
-          }
-        }
-      }
-    `}
+<StaticQuery  
+  query={graphql`
+    query ($locale: String! = "es") { ...TeamPageQuery }
+  `}
   render = {data => (<TeamPageScaffolding content={data.contentfulTeamPage} locale="es" />)}
   />
 );
