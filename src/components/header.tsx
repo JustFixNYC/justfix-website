1b0aecf557517a80abb458f33423270e90fc8b5a
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-03-24 20:57:04 -0400

    small tweaks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ const MoratoriumBanner = () => {
   const [isVisible, setVisibility] = useState(true);
 
   return (
-    <section className={"hero is-warning is-small " + (!isVisible && "is-hidden")}>
+    <section className={"hero is-warning is-small " + (!isVisible ? "is-hidden" : "")}>
       <div className="hero-body">
         <div className="container">
           <button className="delete is-medium is-pulled-right" onClick = {() => setVisibility(false)} />
@@ -31,7 +31,7 @@ const MoratoriumBanner = () => {
               <span className="has-text-weight-bold">COVID-19 Update: </span>
               JustFix.nyc is still in operation, and we are adapting our products to match new rules put in place during the Covid-19 public health crisis. 
               Thanks to organizing from tenant leaders, renters now have stronger protections during this time, including a full halt on eviction cases. 
-              {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" rel="noopener noreferrer">
+              {' '}<a href="https://www.righttocounselnyc.org/moratorium_faq" target="_blank" rel="noopener noreferrer">
                 <span className="has-text-weight-bold">Learn more</span>
               </a>
             </Trans>
