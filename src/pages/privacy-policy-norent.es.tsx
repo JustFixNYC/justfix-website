91f8fe39291e249b6669052e5a04d7820e18f9b6
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-04-24 10:23:38 -0400

    added norent privacy policy page

---
+++ src/pages/privacy-policy-norent.es.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import { NorentPrivacyPolicyPageScaffolding } from "./privacy-policy-norent";
+
+const NorentPrivacyPolicyPage = () => (
+  <StaticQuery
+    query={graphql`
+      query($locale: String! = "es") {
+        ...NorentPrivacyPolicyPage
+      }
+    `}
+    render={(data) => (
+      <NorentPrivacyPolicyPageScaffolding
+        content={data.contentfulGenericPage}
+        locale="es"
+      />
+    )}
+  />
+);
+
+export default NorentPrivacyPolicyPage;
