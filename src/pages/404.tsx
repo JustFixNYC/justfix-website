03e23a4f79da3763dac3e8025fb33cdc5463a308
Sam R <sam@MacBook-Pro.local>
2020-08-11 13:52:29 -0400

    added dummy locale to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { Trans } from "@lingui/macro";
 import Layout from "../components/layout";
 
 const NotFoundPage = () => (
-  <Layout metadata={{ title: "Page Not Found" }}>
+  <Layout metadata={{ title: "Page Not Found" }} locale="en">
     <section className="hero is-large has-background-info">
       <div className="hero-body has-text-centered">
         <h1 className="title has-text-danger is-uppercase">
