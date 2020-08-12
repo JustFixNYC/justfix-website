662259a69f2af0f6c384ef7e916b22a08819ce72
Sam R <sam@MacBook-Pro.local>
2020-08-12 17:37:45 -0400

    refactored header to use new locale hook

--- src/util/use-locale.ts
+++ src/util/use-locale.ts
@@ -5,6 +5,7 @@ import localeConfig from "./locale-config.json";
 export const getLocaleFromPathname = (pathname: string) => {
   let urlBits = pathname.split("/").filter((elem) => !!elem);
   const locale = urlBits[0];
+  console.log(locale);
   if (!localeConfig.ACCEPTED_LOCALES.includes(locale)) {
     throw new Error("Locale parameter in url is not an accepted locale!");
   }
