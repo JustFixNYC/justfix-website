3595b38b5afd81304bf834700bcee9403bc38585
Sam Rabiyah <sraby@users.noreply.github.com>
2020-08-12 17:50:56 -0400

    Added feature flag for i18n redirects (#129)
    This quick PR adds a feature flag that we can use to turn on redirection to any of our locales vs just our default locale of "en."

--- src/components/locale-redirect.tsx
+++ src/components/locale-redirect.tsx
@@ -10,6 +10,8 @@ import "../styles/locale-redirect.scss";
 // Adapted from this very useful StackOverflow post:
 // https://stackoverflow.com/questions/59908989/redirect-based-on-browser-language-in-gatsby
 
+const allowRedirects = process.env.GATSBY_ENABLE_PUBLIC_FACING_I18N === "1";
+
 const getRedirectLanguage = (
   defaultLocale: string,
   acceptedLocales: string[]
@@ -35,10 +37,12 @@ type Props = {
 };
 
 const LocaleRedirectPage = (props: Props) => {
-  const urlLang = getRedirectLanguage(
-    props.pageContext.defaultLocale,
-    props.pageContext.acceptedLocales
-  );
+  const urlLang = allowRedirects
+    ? getRedirectLanguage(
+        props.pageContext.defaultLocale,
+        props.pageContext.acceptedLocales
+      )
+    : props.pageContext.defaultLocale;
   const redirectURL = `/${urlLang}${props.pageContext.slug}`;
 
   useEffect(() => {
