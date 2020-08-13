050920929643372bf03c67e61eeb12b620f8db2a
Atul Varma <varmaa@gmail.com>
2020-08-13 07:15:52 -0400

    Add docs.

--- src/components/locale-link.tsx
+++ src/components/locale-link.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Link, GatsbyLinkProps } from "gatsby";
 import { useCurrentLocale } from "../util/use-locale";
 
+/**
+ * Like Gatsby's <Link>, but it prefixes the passed-in `to` prop with
+ * the current locale.
+ *
+ * Note that this doesn't localize the actual *text* of the link--it only localizes
+ * the path!
+ */
 export function LocaleLink<TState>(
   props: GatsbyLinkProps<TState> & { ref?: any }
 ): JSX.Element {
