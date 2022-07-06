a72e28dcca6e1f0f5440fc6aab0111bbf69baa4b
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-06 15:24:25 -0400

    Fix column spacing issues

--- src/pages/index.en.tsx
+++ src/pages/index.en.tsx
@@ -27,93 +27,99 @@ const shuffleArray = (array: any[]) =>
 export const LandingPageScaffolding = (props: ContentfulContent) => (
   <Layout isLandingPage={true}>
     <div id="home" className="home-page">
-      <div className="columns my-8-mobile mt-12 mb-11">
-        <div className="column is-1" />
-        <div className="column is-10">
-          <h1>{props.content.landingLeadInText}</h1>
+      <div className="my-8-mobile mt-12 mb-11">
+        <div className="columns">
+          <div className="column is-1" />
+          <div className="column is-10">
+            <h1>{props.content.landingLeadInText}</h1>
+          </div>
         </div>
       </div>
 
       <div className="columns is-desktop is-centered">
-        <div className="column is-11 is-12-mobile">
+        <div className="column is-paddingless is-11 is-12-mobile">
           <Img fluid={props.content.landingImage.fluid} alt="" />
         </div>
       </div>
 
-      <div className="columns has-background-black has-text-white">
-        <div className="column is-12 pt-10 pt-8-mobile pb-12 pb-9-mobile">
-          <h1>
-            {props.content.whoWeAreSection}
+      <div className="has-background-black has-text-white">
+        <div className="columns">
+          <div className="column is-12 pt-10 pt-8-mobile pb-12 pb-9-mobile">
+            <h1>
+              {props.content.whoWeAreSection}
+              <Link
+                to={props.content.whoWeAreButton.link}
+                className="button is-primary is-inline-block mt-2 ml-4 is-hidden-touch"
+              >
+                {props.content.whoWeAreButton.title}
+              </Link>
+            </h1>
             <Link
               to={props.content.whoWeAreButton.link}
-              className="button is-primary is-inline-block mt-2 ml-4 is-hidden-touch"
+              className="button is-primary mt-7 is-hidden-desktop"
             >
               {props.content.whoWeAreButton.title}
             </Link>
-          </h1>
-          <Link
-            to={props.content.whoWeAreButton.link}
-            className="button is-primary mt-7 is-hidden-desktop"
-          >
-            {props.content.whoWeAreButton.title}
-          </Link>
+          </div>
         </div>
       </div>
 
       <div
         id="products"
-        className="columns is-multiline has-background-link has-text-black pb-12 pb-6-mobile"
+        className="has-background-link has-text-black pb-12 pb-6-mobile"
       >
-        <div className="column is-12 pt-10 pt-7-mobile pb-9">
-          <h1 className="is-hidden-touch">
-            {props.content.productSectionTitle}
-          </h1>
-          <h2 className="is-hidden-desktop">
-            {props.content.productSectionTitle}
-          </h2>
-          <h3 className="mt-2">{props.content.productSectionSubtitle}</h3>
-        </div>
-        {shuffleArray(props.content.homePageProductBlocks).map(
-          (product: any, i: number) => (
-            <div className="column is-4 is-12-mobile">
-              <div className="jf-card has-background-white p-8 p-6-mobile">
-                <div className="eyebrow is-small mb-5 mb-4-mobile">
-                  {product.productName}
-                </div>
-                <h3 className="mb-6 mb-5-mobile">{product.title}</h3>
-                <div className="mb-6 mb-3-mobile">
-                  {documentToReactComponents(product.descriptionText.json)}
-                </div>
-                <div className="mt-auto">
-                  <div className="mb-6">
-                    {product.location} · {product.language.join(" · ")}
+        <div className="columns is-multiline">
+          <div className="column is-12 pt-10 pt-7-mobile pb-9">
+            <h1 className="is-hidden-touch">
+              {props.content.productSectionTitle}
+            </h1>
+            <h2 className="is-hidden-desktop">
+              {props.content.productSectionTitle}
+            </h2>
+            <h3 className="mt-2">{props.content.productSectionSubtitle}</h3>
+          </div>
+          {shuffleArray(props.content.homePageProductBlocks).map(
+            (product: any, i: number) => (
+              <div className="column is-4 is-12-mobile">
+                <div className="jf-card has-background-white p-8 p-6-mobile">
+                  <div className="eyebrow is-small mb-5 mb-4-mobile">
+                    {product.productName}
                   </div>
+                  <h3 className="mb-6 mb-5-mobile">{product.title}</h3>
+                  <div className="mb-6 mb-3-mobile">
+                    {documentToReactComponents(product.descriptionText.json)}
+                  </div>
+                  <div className="mt-auto">
+                    <div className="mb-6">
+                      {product.location} · {product.language.join(" · ")}
+                    </div>
 
-                  <OutboundLink
-                    href={product.button.link + PRODUCT_CTA_UTM_CODE}
-                    className="button is-primary mb-6 mb-4-mobile"
-                  >
-                    {product.button.title}
-                  </OutboundLink>
+                    <OutboundLink
+                      href={product.button.link + PRODUCT_CTA_UTM_CODE}
+                      className="button is-primary mb-6 mb-4-mobile"
+                    >
+                      {product.button.title}
+                    </OutboundLink>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        )}
-        <div className="column is-4 is-12-mobile">
-          <div className="jf-card has-background-black has-text-white p-8 p-6-mobile">
-            <h3 className="mb-6 mb-5-mobile">
-              {documentToReactComponents(
-                props.content.productIdeaBanner.content.json
-              )}
-            </h3>
+            )
+          )}
+          <div className="column is-4 is-12-mobile">
+            <div className="jf-card has-background-black has-text-white p-8 p-6-mobile">
+              <h3 className="mb-6 mb-5-mobile">
+                {documentToReactComponents(
+                  props.content.productIdeaBanner.content.json
+                )}
+              </h3>
 
-            <OutboundLink
-              href={props.content.productIdeaBanner.button.link}
-              className="button is-primary mb-6 mb-4-mobile mt-auto"
-            >
-              {props.content.productIdeaBanner.button.title}
-            </OutboundLink>
+              <OutboundLink
+                href={props.content.productIdeaBanner.button.link}
+                className="button is-primary mb-6 mb-4-mobile mt-auto"
+              >
+                {props.content.productIdeaBanner.button.title}
+              </OutboundLink>
+            </div>
           </div>
         </div>
       </div>
@@ -132,31 +138,33 @@ export const LandingPageScaffolding = (props: ContentfulContent) => (
         </div>
       </div>
 
-      <div className="columns">
-        <div className="column is-6 is-12-mobile is-flex is-flex-direction-column">
-          <h1 className="mb-6">{props.content.partnershipsSectionTitle}</h1>
-          <div className="has-background-success p-8 pt-11 is-flex-grow-1 is-flex is-flex-direction-column">
-            <h2 className="mb-11">
-              {props.content.partnershipsSectionSubtitle}
-            </h2>
-            <OutboundLink
-              href={props.content.partnershipsSectionButton.link}
-              className="button is-primary mt-auto is-align-self-flex-start"
-            >
-              {props.content.partnershipsSectionButton.title}
-            </OutboundLink>
+      <div className="mb-12">
+        <div className="columns">
+          <div className="column is-6 is-12-mobile is-flex is-flex-direction-column">
+            <h1 className="mb-6">{props.content.partnershipsSectionTitle}</h1>
+            <div className="has-background-success p-8 pt-11 is-flex-grow-1 is-flex is-flex-direction-column">
+              <h2 className="mb-11">
+                {props.content.partnershipsSectionSubtitle}
+              </h2>
+              <OutboundLink
+                href={props.content.partnershipsSectionButton.link}
+                className="button is-primary mt-auto is-align-self-flex-start"
+              >
+                {props.content.partnershipsSectionButton.title}
+              </OutboundLink>
+            </div>
           </div>
-        </div>
-        <div className="column is-6 is-12-mobile is-flex is-flex-direction-column">
-          <h1 className="mb-6">{props.content.policySectionTitle}</h1>
-          <div className="has-background-link p-8 pt-11 is-flex-grow-1 is-flex is-flex-direction-column">
-            <h2 className="mb-11">{props.content.policySectionSubtitle}</h2>
-            <OutboundLink
-              href={props.content.policySectionButton.link}
-              className="button is-primary mt-auto is-align-self-flex-start"
-            >
-              {props.content.policySectionButton.title}
-            </OutboundLink>
+          <div className="column is-6 is-12-mobile is-flex is-flex-direction-column">
+            <h1 className="mb-6">{props.content.policySectionTitle}</h1>
+            <div className="has-background-link p-8 pt-11 is-flex-grow-1 is-flex is-flex-direction-column">
+              <h2 className="mb-11">{props.content.policySectionSubtitle}</h2>
+              <OutboundLink
+                href={props.content.policySectionButton.link}
+                className="button is-primary mt-auto is-align-self-flex-start"
+              >
+                {props.content.policySectionButton.title}
+              </OutboundLink>
+            </div>
           </div>
         </div>
       </div>
