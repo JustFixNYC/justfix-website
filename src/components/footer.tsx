6ddb4fb693dce9e546c04d8b9b1e0fc073b0b7d0
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-07 11:21:35 -0400

    Add links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -37,99 +37,66 @@ const FooterLanguageToggle = () => {
 
 const Footer = withI18n()(({ i18n }: withI18nProps) => {
   return (
-    <div className="footer has-background-black has-text-white">
-      <div className="columns has-text-centered-touch is-desktop">
-        <div className="column">
+    <div className="jf-footer has-background-black has-text-white py-7">
+      <div className="columns">
+        <div className="column is-9 is-12-touch pt-9">
           <FooterLanguageToggle />
-          <p className="">
-            <Trans>What we do</Trans>
-          </p>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/#products"
-          >
-            <p>
-              <Trans>Products</Trans>
-            </p>
-          </Link>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/learn"
-          >
-            <p>
-              <Trans>Learn</Trans>
-            </p>
-          </Link>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/our-mission"
-          >
-            <p>
-              <Trans>Mission</Trans>
-            </p>
-          </Link>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/press"
-          >
-            <p>
-              <Trans>Press</Trans>
-            </p>
-          </Link>
+
+          {/* TODO: Update these footer links with actual links to internal pages */}
+          <div className="columns is-paddingless is-hidden-touch">
+            <div className="column is-3 is-paddingless mx-0">
+              <Link className="no-underline" to="/#products">
+                <p className="title is-4 has-text-white">
+                  <Trans>Products</Trans>
+                </p>
+              </Link>
+              <Link className="no-underline" to="/learn">
+                <p className="title is-4 has-text-white">
+                  <Trans>Learn</Trans>
+                </p>
+              </Link>
+            </div>
+            <div className="column is-3 is-paddingless">
+              <Link className="no-underline" to="/#products">
+                <p className="title is-4 has-text-white">
+                  <Trans>Products</Trans>
+                </p>
+              </Link>
+              <Link className="no-underline" to="/learn">
+                <p className="title is-4 has-text-white">
+                  <Trans>Learn</Trans>
+                </p>
+              </Link>
+            </div>
+            <div className="column is-3 is-paddingless">
+              <Link className="no-underline" to="/#products">
+                <p className="title is-4 has-text-white">
+                  <Trans>Products</Trans>
+                </p>
+              </Link>
+              <Link className="no-underline" to="/learn">
+                <p className="title is-4 has-text-white">
+                  <Trans>Learn</Trans>
+                </p>
+              </Link>
+            </div>
+            <div className="column is-3 is-paddingless">
+              <Link className="no-underline" to="/#products">
+                <p className="title is-4 has-text-white">
+                  <Trans>Products</Trans>
+                </p>
+              </Link>
+              <Link className="no-underline" to="/learn">
+                <p className="title is-4 has-text-white">
+                  <Trans>Learn</Trans>
+                </p>
+              </Link>
+            </div>
+          </div>
         </div>
 
-        <div className="column">
-          <p className="link-header has-text-white has-text-weight-bold is-uppercase">
-            <Trans>About us</Trans>
-          </p>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/team"
-          >
-            <p>
-              <Trans>Team</Trans>
-            </p>
-          </Link>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/partners"
-          >
-            <p>
-              <Trans>Partners</Trans>
-            </p>
-          </Link>
-          <a
-            className="link has-text-weight-semibold is-uppercase"
-            href="https://justfix.breezy.hr/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>
-              <Trans>Jobs</Trans>
-            </p>
-          </a>
-          <Link
-            className="link has-text-weight-semibold is-uppercase"
-            to="/contact-us"
-          >
-            <p>
-              <Trans>Contact Us</Trans>
-            </p>
-          </Link>
-          <a
-            className="link has-text-weight-semibold is-uppercase"
-            href="https://donorbox.org/donate-to-justfix-nyc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>
-              <Trans>Donate</Trans>
-            </p>
-          </a>
-        </div>
-
-        <div className="column">
-          <h4 className="title is-size-5 has-text-white">
+        <div className="column is-paddingless">
+          <h4 className="mb-4">
             <Trans>Join our mailing list!</Trans>
           </h4>
           <Subscribe />
@@ -159,7 +126,6 @@ const Footer = withI18n()(({ i18n }: withI18nProps) => {
               style={{ height: 40, width: 40 }}
             />
             <SocialIcon
-              className="mr-3"
               url="https://github.com/JustFixNYC"
               target="_blank"
               rel="noopener noreferrer"
