2b87d42d51072924f965c6a8464482734dbbaed8
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-19 14:44:06 -0400

    Add alternative format for the PageHero component  (#228)
    This PR refactors our <PageHero> component to accept an optional image property and then render an alternative layout of the component to show the image.

--- src/components/page-hero.tsx
+++ src/components/page-hero.tsx
@@ -1,43 +1,73 @@
 import React from "react";
 import { Trans } from "@lingui/macro";
+import BackgroundImage from "gatsby-background-image";
+import ResponsiveElement from "./responsive-element";
 
 type PageHeroInfo = {
   pageName: string;
   description: string;
   onThisPageList: string[];
+  image?: {
+    fluid: any;
+  };
 };
 
 const PageHero: React.FC<PageHeroInfo> = ({
   pageName,
   description,
   onThisPageList,
+  image,
 }) => (
   <div className="jf-page-hero is-flex is-justify-content-flex-end has-background-black has-text-white">
-    <div className="columns is-marginless is-paddingless">
-      <div className="column is-9 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
-        <div>
-          <div className="eyebrow is-large pb-4">{pageName}</div>
-          <h1 className="mt-4">{description}</h1>
-        </div>
-      </div>
-      <div className="column is-3 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
-        <div>
-          <div className="eyebrow is-large pb-4">
-            <Trans>On this page</Trans>
+    {image ? (
+      <div className="columns is-marginless is-paddingless">
+        <BackgroundImage
+          className="column is-6"
+          fluid={image.fluid}
+          style={{
+            backgroundPosition: "center bottom",
+          }}
+        />
+        <div className="column is-6 px-10 py-9 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <ResponsiveElement
+              desktop="h2"
+              touch="h1"
+              className="mb-9 mb-5-mobile"
+            >
+              {description}
+            </ResponsiveElement>
+            <div className="eyebrow is-large pt-4">{pageName}</div>
           </div>
-          <ul className="mt-4">
-            {onThisPageList.map((item: string, i: number) => (
-              <li
-                className="title is-3 has-text-white is-marginless pl-8"
-                key={i}
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
-    </div>
+    ) : (
+      <div className="columns is-marginless is-paddingless">
+        <div className="column is-9 px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <div className="eyebrow is-large pb-4">{pageName}</div>
+            <h1 className="mt-4">{description}</h1>
+          </div>
+        </div>
+        <div className="column is-3 px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <div className="eyebrow is-large pb-4">
+              <Trans>On this page</Trans>
+            </div>
+            <ul className="mt-4">
+              {onThisPageList.map((item: string, i: number) => (
+                <li
+                  className="title is-3 has-text-white is-marginless pl-8"
+                  key={i}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )}
   </div>
 );
 
