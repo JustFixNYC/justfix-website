c17debcf14990577448fee820e0439a95921063d
Maxwell Austensen <maxwell.austensen@nyu.edu>
2022-07-19 15:31:58 +0000

    create responsive-element component

---
+++ src/components/responsive-element.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import classnames from "classnames";
+
+type ResponsiveElementTagName = "h1" | "h2" | "h3" | "h4" | "p";
+
+type ResponsiveElementInfo = {
+  desktop: ResponsiveElementTagName;
+  touch: ResponsiveElementTagName;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const ResponsiveElement = ({
+  desktop,
+  touch,
+  children,
+  className,
+}: ResponsiveElementInfo) => {
+  const Desktop = desktop as keyof JSX.IntrinsicElements;
+  const Touch = touch as keyof JSX.IntrinsicElements;
+  // console.log(Touch instanceof)
+  return (
+    <>
+      <Desktop className={classnames("is-hidden-touch", className || "")}>
+        {children}
+      </Desktop>
+      <Touch className={classnames("is-hidden-desktop", className || "")}>
+        {children}
+      </Touch>
+    </>
+  );
+};
+
+export default ResponsiveElement;
