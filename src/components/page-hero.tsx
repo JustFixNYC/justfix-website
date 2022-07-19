e15b14e20219e48bb7e05a5ab6984a605a96f43b
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-19 13:33:10 -0400

    Add content to right side of image hero

--- src/components/page-hero.tsx
+++ src/components/page-hero.tsx
@@ -18,41 +18,49 @@ const PageHero: React.FC<PageHeroInfo> = ({
   image,
 }) => (
   <div className="jf-page-hero is-flex is-justify-content-flex-end has-background-black has-text-white">
-    <div className="columns is-marginless is-paddingless">
-      {image ? (
+    {image ? (
+      <div className="columns is-marginless is-paddingless">
         <BackgroundImage
-          className="column is-6 is-12-mobile"
+          className="column is-6"
           fluid={image.fluid}
           style={{
             backgroundPosition: "center bottom",
           }}
         />
-      ) : (
-        <div className="column is-9 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+        <div className="column is-6 px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
           <div>
             <div className="eyebrow is-large pb-4">{pageName}</div>
             <h1 className="mt-4">{description}</h1>
           </div>
         </div>
-      )}
-      <div className="column is-3 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
-        <div>
-          <div className="eyebrow is-large pb-4">
-            <Trans>On this page</Trans>
+      </div>
+    ) : (
+      <div className="columns is-marginless is-paddingless">
+        <div className="column is-9 px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <div className="eyebrow is-large pb-4">{pageName}</div>
+            <h1 className="mt-4">{description}</h1>
           </div>
-          <ul className="mt-4">
-            {onThisPageList.map((item: string, i: number) => (
-              <li
-                className="title is-3 has-text-white is-marginless pl-8"
-                key={i}
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
+        </div>
+        <div className="column is-3 px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <div className="eyebrow is-large pb-4">
+              <Trans>On this page</Trans>
+            </div>
+            <ul className="mt-4">
+              {onThisPageList.map((item: string, i: number) => (
+                <li
+                  className="title is-3 has-text-white is-marginless pl-8"
+                  key={i}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    )}
   </div>
 );
 
