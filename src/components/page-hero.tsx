f5ef5473f0fbfb9706ca98142467633688c078b9
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-19 13:28:56 -0400

    Add potential background image to page hero

--- src/components/page-hero.tsx
+++ src/components/page-hero.tsx
@@ -1,25 +1,40 @@
 import React from "react";
 import { Trans } from "@lingui/macro";
+import BackgroundImage from "gatsby-background-image";
 
 type PageHeroInfo = {
   pageName: string;
   description: string;
   onThisPageList: string[];
+  image?: {
+    fluid: any;
+  };
 };
 
 const PageHero: React.FC<PageHeroInfo> = ({
   pageName,
   description,
   onThisPageList,
+  image,
 }) => (
   <div className="jf-page-hero is-flex is-justify-content-flex-end has-background-black has-text-white">
     <div className="columns is-marginless is-paddingless">
-      <div className="column is-9 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
-        <div>
-          <div className="eyebrow is-large pb-4">{pageName}</div>
-          <h1 className="mt-4">{description}</h1>
+      {image ? (
+        <BackgroundImage
+          className="column is-6 is-12-mobile"
+          fluid={image.fluid}
+          style={{
+            backgroundPosition: "center bottom",
+          }}
+        />
+      ) : (
+        <div className="column is-9 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
+          <div>
+            <div className="eyebrow is-large pb-4">{pageName}</div>
+            <h1 className="mt-4">{description}</h1>
+          </div>
         </div>
-      </div>
+      )}
       <div className="column is-3 is-12-mobile px-9 py-10 p-6-mobile is-flex is-align-items-flex-end">
         <div>
           <div className="eyebrow is-large pb-4">
