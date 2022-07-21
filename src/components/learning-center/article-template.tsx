16464bd963174ff19b2c6a151bc29504754d75f7
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-21 16:04:15 -0400

    Add first part of learning center article styling

--- src/components/learning-center/article-template.tsx
+++ src/components/learning-center/article-template.tsx
@@ -5,11 +5,11 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import "../../styles/learn.scss";
 import { Trans } from "@lingui/macro";
-import { LocaleLink } from "../locale-link";
 import { useCurrentLocale } from "../../util/use-locale";
-import Img from "gatsby-image/withIEPolyfill";
 import { EmbeddedAsset } from "../embedded-asset-node";
 import { BLOCKS } from "@contentful/rich-text-types";
+import { LocaleLink } from "../locale-link";
+import ResponsiveElement from "../responsive-element";
 
 const widont = require("widont");
 
@@ -76,40 +76,31 @@ function renderSection(articleSection: any, i: number): JSX.Element {
           )}
         </div>
       ) : (
-        <div className="content">
-          <h1 className="title is-size-3 is-size-4-mobile has-text-grey-dark has-text-weight-semibold is-spaced">
-            {widont(articleSection.title)}
-          </h1>
-          <span className="has-text-grey-dark">
-            {documentToReactComponents(articleSection.content.json, {
-              renderNode: {
-                [BLOCKS.EMBEDDED_ASSET]: (eaNode) => (
-                  <p className="mt-5">
-                    <EmbeddedAsset node={eaNode} locale={locale} />
-                  </p>
-                ),
-              },
-            })}
-          </span>
+        <div>
+          <h3 className="my-6 my-5-mobile">{widont(articleSection.title)}</h3>
+          {documentToReactComponents(articleSection.content.json, {
+            renderNode: {
+              [BLOCKS.PARAGRAPH]: (node, children) => (
+                <p className="title is-4">{children}</p>
+              ),
+              [BLOCKS.EMBEDDED_ASSET]: (eaNode) => (
+                <p className="mt-5">
+                  <EmbeddedAsset node={eaNode} locale={locale} />
+                </p>
+              ),
+              [BLOCKS.UL_LIST]: (node, children) => (
+                <ul className="ml-8 mb-6">{children}</ul>
+              ),
+            },
+          })}
         </div>
       )}
-      <ScrollLink
-        to="navmenu"
-        spy={true}
-        smooth={true}
-        offset={-100}
-        duration={500}
-        className="back-to-top is-uppercase is-hidden-desktop is-size-7 has-text-weight-semibold has-letters-spaced"
-      >
-        <Trans>Back to top</Trans> ↑
-      </ScrollLink>
     </div>
   );
 }
 
 const LearningArticle = (props: Props) => {
   const content = props.pageContext.content;
-  const locale = useCurrentLocale();
 
   const NavMenu = (props?: navMenuProps) => (
     <aside
@@ -145,95 +136,87 @@ const LearningArticle = (props: Props) => {
 
   return (
     <Layout metadata={content.metadata}>
-      <div className="article-page">
-        <div className="columns is-desktop">
-          <div className="column" />
-          <div className="column is-half-desktop">
-            <div className="hero">
-              <div className="hero-body">
-                <nav className="breadcrumb" aria-label="breadcrumbs">
-                  <ul>
-                    <li>
-                      <LocaleLink to="/learn/">
-                        {props.pageContext.learningCenterTitle}
-                      </LocaleLink>
-                    </li>
-                    <li>
-                      <LocaleLink
-                        to={
-                          "/learn/category/" + content.categories[0].slug + "/"
-                        }
-                      >
-                        {content.categories[0].title}
-                      </LocaleLink>
-                    </li>
-                  </ul>
-                </nav>
-                <div className="container content-wrapper">
-                  {content.coverPhoto && (
-                    <div className="container">
-                      <figure className="image is-horizontal-center">
-                        <Img
-                          fluid={content.coverPhoto.fluid}
-                          alt={content.title}
-                        />
-                      </figure>
-                      <br />
-                    </div>
-                  )}
-                  {locale === "es" && content.englishOnly && (
-                    <p className="has-text-danger is-italic">Solo en inglés</p>
-                  )}
-                  <h1 className="title is-size-2 is-size-3-mobile has-text-grey-dark has-text-weight-semibold is-spaced">
-                    {widont(content.title)}
-                  </h1>
-                  <p className="subtitle is-size-6 has-text-grey-dark">
-                    <span className="is-size-6">
-                      <Trans>Written by</Trans> {content.author}
-                    </span>
-                    <br />
-                    <span className="is-size-7 is-uppercase">
-                      <Trans>Updated</Trans> {content.dateUpdated}
-                    </span>
-                  </p>
-                  {content.subtitle && (
-                    <div className="content is-size-6 has-text-grey-dark">
-                      {documentToReactComponents(content.subtitle.json)}
-                    </div>
-                  )}
-                </div>
+      <div className="jf-article-page">
+        <div className="columns is-centered">
+          <div className="column is-8 pt-12 pt-7-mobile">
+            <nav className="breadcrumb is-marginless" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <LocaleLink
+                    to="/learn/"
+                    className="eyebrow is-large has-text-black no-underline"
+                  >
+                    <Trans>Learning Center</Trans>
+                  </LocaleLink>
+                </li>
+                {/* Note: we need to add the eyebrow css class to the <li> 
+                    component to style the "/" separator */}
+                <li className="eyebrow is-large">
+                  <LocaleLink
+                    to={"/learn/category/" + content.categories[0].slug + "/"}
+                    className="eyebrow is-large has-text-black no-underline"
+                  >
+                    {content.categories[0].title}
+                  </LocaleLink>
+                </li>
+              </ul>
+            </nav>
+
+            <ResponsiveElement
+              desktop="h2"
+              touch="h1"
+              className="mt-2 mb-6 my-5-mobile"
+            >
+              {content.title}
+            </ResponsiveElement>
+
+            <div className="eyebrow is-small mb-6 mb-5-mobile">
+              <Trans>Published</Trans> {content.dateUpdated}
+            </div>
+
+            <p className="has-text-dark mb-6 mb-8-mobile">
+              {content.author || (
+                <Trans>
+                  This article was written by the team of NYC-based housing
+                  experts at nonprofit JustFix
+                </Trans>
+              )}
+            </p>
+
+            <div className="columns is-paddingless has-background-light">
+              <div className="column is-6 p-5">
+                <h4>
+                  <Trans>Summary</Trans>
+                </h4>
+                <p>{content.summary}</p>
+              </div>
+              <div className="column is-6 p-5">
+                <h4>
+                  <Trans>What can I do?</Trans>
+                </h4>
+                <p>{documentToReactComponents(content.whatCanIDo.json)}</p>
               </div>
             </div>
+
+            <div className="title is-4 mt-7">
+              {documentToReactComponents(content.subtitle.json, {
+                renderNode: {
+                  [BLOCKS.PARAGRAPH]: (node, children) => (
+                    <p className="title is-4">{children}</p>
+                  ),
+                },
+              })}
+            </div>
           </div>
-          <div className="column" />
         </div>
-        <div className="columns is-desktop">
+        <div className="columns">
           <div className="column">
             <NavMenu styleClass="is-hidden-desktop" />
           </div>
-          <div className="column is-half-desktop">
-            <div className="hero-body">
-              <div className="container content-wrapper">
-                {content.articleSections.map(
-                  (articleSection: any, i: number) => {
-                    const includeDivider =
-                      i > 0 &&
-                      content.articleSections[i].__typename ===
-                        "ContentfulLearningArticleSection" &&
-                      content.articleSections[i - 1].__typename ===
-                        "ContentfulLearningArticleSection";
-                    return (
-                      <div key={i}>
-                        {includeDivider && (
-                          <div className="is-divider light is-hidden-touch" />
-                        )}
-                        {renderSection(articleSection, i)}
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-            </div>
+          <div className="column is-8">
+            {content.articleSections.map((articleSection: any, i: number) => (
+              <div key={i}>{renderSection(articleSection, i)}</div>
+            ))}
           </div>
           <div className="column">
             <NavMenu styleClass="sticky is-hidden-touch" />
