c87a2d7a3bf41822cc10533c7af77f6752b9172d
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-22 13:02:05 -0400

    Mobile optimization

--- src/components/cookies-banner.tsx
+++ src/components/cookies-banner.tsx
@@ -30,16 +30,16 @@ export const CookiesBanner = () => {
     >
       <div className="is-flex is-align-items-center">
         <div className="p-6">
-          <Trans>By using our website you consent to the use of cookies.</Trans>
+          <Trans>By using our website you consent to the use of cookies.</Trans>{" "}
+          <div
+            role="button"
+            className="link ml-2"
+            aria-expanded="false"
+            onClick={() => setModalActive(true)}
+          >
+            <Trans>Learn more</Trans>
+          </div>
         </div>
-        <button
-          role="button"
-          className="button is-text"
-          aria-expanded="false"
-          onClick={() => setModalActive(true)}
-        >
-          <Trans>Learn more</Trans>
-        </button>
       </div>
       <div className="pr-6 is-flex is-align-items-center">
         <button
@@ -49,7 +49,7 @@ export const CookiesBanner = () => {
           aria-expanded="false"
           onClick={handleClick}
         >
-          <img src={require("../img/close.svg")} width="14" height="14" />
+          <img src={require("../img/close.svg")} width="14px" height="14px" />
         </button>
       </div>
       <div className={classnames("modal", modalIsActive && "is-active")}>
@@ -61,34 +61,36 @@ export const CookiesBanner = () => {
             clickOutsideDeactivates: true,
           }}
         >
-          <div className="modal-content has-background-white p-6">
-            <div className="has-text-centered mb-5">
-              <h3>
-                <Trans>We use cookies on this website</Trans>
-              </h3>
-            </div>
-            <p className="mb-5">
-              <Trans>
-                This means that we’re able to see your approximate location, the
-                type of device you’re using, and your operating system.
-              </Trans>
-            </p>
-            <p className="is-small mb-5">
-              <Trans>
-                These cookies track the performance of our tools and for
-                marketing purposes. This data allows us to improve our tools to
-                give you the best experience.
-              </Trans>
-            </p>
-            <div className="buttons is-centered">
-              <button
-                className="button is-primary"
-                role="button"
-                aria-label="close"
-                onClick={() => setModalActive(false)}
-              >
-                <Trans>Got it</Trans>
-              </button>
+          <div className="modal-content">
+            <div className="has-background-white p-6 mx-6">
+              <div className="has-text-centered mb-5">
+                <h3>
+                  <Trans>We use cookies on this website</Trans>
+                </h3>
+              </div>
+              <p className="mb-5">
+                <Trans>
+                  This means that we’re able to see your approximate location,
+                  the type of device you’re using, and your operating system.
+                </Trans>
+              </p>
+              <p className="is-small mb-5">
+                <Trans>
+                  These cookies track the performance of our tools and for
+                  marketing purposes. This data allows us to improve our tools
+                  to give you the best experience.
+                </Trans>
+              </p>
+              <div className="buttons is-centered">
+                <button
+                  className="button is-primary"
+                  role="button"
+                  aria-label="close"
+                  onClick={() => setModalActive(false)}
+                >
+                  <Trans>Got it</Trans>
+                </button>
+              </div>
             </div>
           </div>
         </FocusTrap>
