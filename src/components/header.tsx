c078e8e11f14615d80a9d44791717464d37360c3
Maxwell Austensen <m.austensen@gmail.com>
2022-07-27 11:53:42 -0400

    Navbar scroll (#239)
    This PR makes the header/navbar appear fixed at the top of the page when the user scrolls up. This only applies for mobile. This PR doesn't include a smooth transition yet, but that hopefully will be added in a follow up PR soon.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Trans } from "@lingui/macro";
 
 import "../styles/header.scss";
@@ -11,6 +11,7 @@ import { INLINES } from "@contentful/rich-text-types";
 import { OutboundLink } from "gatsby-plugin-google-analytics";
 import FocusTrap from "focus-trap-react";
 import { FooterLanguageToggle } from "./footer";
+import classnames from "classnames";
 
 const commonStrings = new ContentfulCommonStrings(_commonStrings as any);
 
@@ -31,6 +32,43 @@ export const SITE_LINKS: LinkWithLabel[] = [
   [CAREERS_PAGE_URL, <Trans>Careers</Trans>],
   ["/contact-us", <Trans>Contact Us</Trans>],
 ];
+
+/** Hook to get scroll direction for sticky banner */
+// https://stackoverflow.com/a/62497293/7051239
+const useScrollDirection = (): "up" | "down" => {
+  const [scrollDir, setScrollDir] = useState<"up" | "down">("down");
+
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(scrollY > lastScrollY ? "down" : "up");
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollDir]);
+
+  return scrollDir;
+};
 
 const MoratoriumBanner: React.FC<{}> = () => {
   const [isVisible, setVisibility] = useState(true);
@@ -82,112 +120,131 @@ const Header: React.FC<{
   isLandingPage?: boolean;
 }> = ({ isLandingPage }) => {
   const [burgerMenuIsOpen, setBurgerMenuStatus] = useState(false);
+  const isScrollingUp = useScrollDirection() === "up";
 
   return (
-    <div className="header">
-      {isLandingPage && <MoratoriumBanner />}
-      <FocusTrap
-        active={burgerMenuIsOpen}
-        focusTrapOptions={{
-          onDeactivate: () => setBurgerMenuStatus(false),
-          clickOutsideDeactivates: true,
-        }}
-      >
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-          <div className="navbar-brand">
-            <div
-              className="navbar-item is-flex-direction-column is-justify-content-center no-underline"
-              onClick={() => setBurgerMenuStatus(false)}
-            >
-              <Link to="/">
-                <img
-                  className="is-hidden-touch"
-                  src={require("../img/brand/logo.svg")}
-                  width="164"
-                  height="38"
-                  alt="JustFix"
-                />
-                <img
-                  className="is-hidden-desktop"
-                  src={require("../img/brand/logo.svg")}
-                  width="120"
-                  height="27"
-                  alt="JustFix"
-                />
-              </Link>
-              {isDemoSite && (
-                <span className="tag is-yellow">
-                  <Trans>DEMO SITE</Trans>
-                </span>
-              )}
-            </div>
-            <div
-              className="navbar-item is-size-3 has-text-black is-hidden-touch"
-              onClick={() => setBurgerMenuStatus(false)}
-            >
-              <Trans>Technology for Housing Justice</Trans>
-            </div>
-            <div
-              className="navbar-item is-paddingless is-flex-grow-1"
-              onClick={() => setBurgerMenuStatus(false)}
-            />
-            <div
-              className="navbar-item is-hidden-touch"
-              onClick={() => setBurgerMenuStatus(false)}
-            >
-              <Link to="/tools" className="button is-primary">
-                <Trans>See our tools</Trans>
-              </Link>
-            </div>
-            <div className={"navbar-item " + (burgerMenuIsOpen && "is-active")}>
-              <button
-                role="button"
-                className={
-                  "navbar-burger burger " + (burgerMenuIsOpen && "is-active")
-                }
-                aria-expanded="false"
-                onClick={() => setBurgerMenuStatus(!burgerMenuIsOpen)}
-                data-target="navbar"
-              >
-                <img
-                  src={
-                    burgerMenuIsOpen
-                      ? require("../img/close.svg")
-                      : require("../img/menu.svg")
-                  }
-                  className="mr-3"
-                  width="16"
-                  height="12"
-                />
-                {burgerMenuIsOpen ? <Trans>Close</Trans> : <Trans>Menu</Trans>}
-              </button>
-            </div>
-          </div>
-
-          <div
-            id="main-navbar-menu"
-            className={
-              "navbar-menu has-background-black px-1-mobile " +
-              (burgerMenuIsOpen && "is-active")
-            }
+    <>
+      <div
+        className={classnames(
+          "jf-sticky-phantom is-hidden-desktop",
+          isScrollingUp && "jf-sticky"
+        )}
+      />
+      <div className={classnames("header", isScrollingUp && "jf-sticky")}>
+        {isLandingPage && <MoratoriumBanner />}
+        <FocusTrap
+          active={burgerMenuIsOpen}
+          focusTrapOptions={{
+            onDeactivate: () => setBurgerMenuStatus(false),
+            clickOutsideDeactivates: true,
+          }}
+        >
+          <nav
+            className="navbar"
+            role="navigation"
+            aria-label="main navigation"
           >
-            <div className="navbar-end is-flex is-flex-direction-column is-justify-content-space-between py-3 px-5">
-              <div>
-                {SITE_LINKS.map((link, i) => (
-                  <HeaderLink link={link} key={i} />
-                ))}
+            <div className="navbar-brand">
+              <div
+                className="navbar-item is-flex-direction-column is-justify-content-center no-underline"
+                onClick={() => setBurgerMenuStatus(false)}
+              >
+                <Link to="/">
+                  <img
+                    className="is-hidden-touch"
+                    src={require("../img/brand/logo.svg")}
+                    width="164"
+                    height="38"
+                    alt="JustFix"
+                  />
+                  <img
+                    className="is-hidden-desktop"
+                    src={require("../img/brand/logo.svg")}
+                    width="120"
+                    height="27"
+                    alt="JustFix"
+                  />
+                </Link>
+                {isDemoSite && (
+                  <span className="tag is-yellow">
+                    <Trans>DEMO SITE</Trans>
+                  </span>
+                )}
               </div>
+              <div
+                className="navbar-item is-size-3 has-text-black is-hidden-touch"
+                onClick={() => setBurgerMenuStatus(false)}
+              >
+                <Trans>Technology for Housing Justice</Trans>
+              </div>
+              <div
+                className="navbar-item is-paddingless is-flex-grow-1"
+                onClick={() => setBurgerMenuStatus(false)}
+              />
+              <div
+                className="navbar-item is-hidden-touch"
+                onClick={() => setBurgerMenuStatus(false)}
+              >
+                <Link to="/tools" className="button is-primary">
+                  <Trans>See our tools</Trans>
+                </Link>
+              </div>
+              <div
+                className={"navbar-item " + (burgerMenuIsOpen && "is-active")}
+              >
+                <button
+                  role="button"
+                  className={
+                    "navbar-burger burger " + (burgerMenuIsOpen && "is-active")
+                  }
+                  aria-expanded="false"
+                  onClick={() => setBurgerMenuStatus(!burgerMenuIsOpen)}
+                  data-target="navbar"
+                >
+                  <img
+                    src={
+                      burgerMenuIsOpen
+                        ? require("../img/close.svg")
+                        : require("../img/menu.svg")
+                    }
+                    className="mr-3"
+                    width="16"
+                    height="12"
+                  />
+                  {burgerMenuIsOpen ? (
+                    <Trans>Close</Trans>
+                  ) : (
+                    <Trans>Menu</Trans>
+                  )}
+                </button>
+              </div>
+            </div>
 
-              <div className="navbar-item has-dropdown is-hoverable mt-7 mb-4 mb-7-mobile">
-                <div className="navbar-dropdown is-right pt-1 pb-0">
-                  <FooterLanguageToggle />
+            <div
+              id="main-navbar-menu"
+              className={
+                "navbar-menu has-background-black px-1-mobile " +
+                (burgerMenuIsOpen && "is-active")
+              }
+            >
+              <div className="navbar-end is-flex is-flex-direction-column is-justify-content-space-between py-3 px-5">
+                <div>
+                  {SITE_LINKS.map((link, i) => (
+                    <HeaderLink link={link} key={i} />
+                  ))}
+                </div>
+
+                <div className="navbar-item has-dropdown is-hoverable mt-7 mb-4 mb-7-mobile">
+                  <div className="navbar-dropdown is-right pt-1 pb-0">
+                    <FooterLanguageToggle />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </nav>
-      </FocusTrap>
-    </div>
+          </nav>
+        </FocusTrap>
+      </div>
+    </>
   );
 };
 
