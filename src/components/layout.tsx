967a84ec9a2c9babbaea53f971687f014f99e4bc
Sam Rabiyah <sraby@users.noreply.github.com>
2022-07-27 11:59:17 -0400

    Add fixed button to bottom of viewport on mobile homepage (#237)
    This PR implements a fixed footer menu with a button that links to our tools page, only visible on mobile and tablet devices on the homepage.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,9 +7,13 @@ import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import Footer from "./footer";
-import { useCurrentLocale } from "../util/use-locale";
+import { removeLocaleFromPathname, useCurrentLocale } from "../util/use-locale";
 import localeConfig from "../util/locale-config.json";
 import { CookiesBanner } from "./cookies-banner";
+import { useLocation } from "@reach/router";
+import { LocaleLink } from "./locale-link";
+import { Trans } from "@lingui/macro";
+import classnames from "classnames";
 
 const favicon16 = require("../img/brand/favicon-16x16.png");
 const favicon32 = require("../img/brand/favicon-32x32.png");
@@ -34,8 +38,6 @@ const catalogs: LocaleCatalogs = {
 
 export const formatImageUrlForSEO = (url: string) =>
   url.startsWith("//") ? encodeURI(`https:${url}`) : encodeURI(url);
-
-// import './layout.css'
 
 type Props = {
   metadata?: {
@@ -75,6 +77,8 @@ const LayoutScaffolding = ({
   }
 
   const locale = useCurrentLocale() || localeConfig.DEFAULT_LOCALE;
+  const { pathname } = useLocation();
+  const isHomepage = removeLocaleFromPathname(pathname) === "";
 
   return (
     <I18nProvider language={locale} catalogs={catalogs}>
@@ -119,9 +123,25 @@ const LayoutScaffolding = ({
         <meta name="twitter:image:alt" content={title} />
       </Helmet>
       <Header isLandingPage={isLandingPage} />
-      <div>{children}</div>
-      <Footer />
-      <CookiesBanner />
+      <div
+        className={
+          // Add extra space at bottom of page for fixed footer button:
+          classnames(isHomepage && "mb-12-mobile")
+        }
+      >
+        {children}
+        <Footer />
+        <div className="jf-footer-menu">
+          <CookiesBanner />
+          {isHomepage && (
+            <div className="has-background-black py-5 is-flex is-justify-content-center is-hidden-desktop">
+              <LocaleLink to="/tools" className="button is-primary">
+                <Trans>See our tools</Trans>
+              </LocaleLink>
+            </div>
+          )}
+        </div>
+      </div>
     </I18nProvider>
   );
 };
